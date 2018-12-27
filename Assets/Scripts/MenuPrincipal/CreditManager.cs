57eb14472655062220ffa6d33178fb4094008aaa
setseroZZ <resetos35@gmail.com>
2018-12-27 14:54:54 -0500

    Con UI de menu principal, creditos y pantalla de pausa

---
+++ Assets/Scripts/MenuPrincipal/CreditManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditManager : MonoBehaviour {
+    public string menuScene;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            LoadMenu();
+        }
+	}
+
+
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(menuScene, LoadSceneMode.Single);
+    }
+    
+}
