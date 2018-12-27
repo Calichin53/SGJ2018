0280e71f3624455536ba00823a0c4dcf7ac6ad3f
Jose Saldaña <josesaldanavi@gmail.com>
2018-12-27 13:44:50 -0500

    SceneNavMesh

---
+++ Assets/Scripts/Enemy/EnemyNavMesh.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavMesh : MonoBehaviour {
+    public Transform towerPrincipal;
+    NavMeshAgent creep;
+    private bool dentro = false;
+
+    public NexoController poinLife;
+	// Use this for initialization
+	void Start () {
+        creep = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!dentro)
+        {
+            creep.destination = towerPrincipal.position;
+            
+        }
+        if (dentro)
+        {
+            
+            creep.destination = this.transform.position;
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            dentro = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            dentro = false;
+        }
+    }
+
+}
