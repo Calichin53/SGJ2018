a46abe99f748f05e77d56d24813936c1fa734ada
Jose Saldaña <josesaldanavi@gmail.com>
2018-12-27 04:13:35 -0500

    Enemy spawn and enemy movement

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public float speed;
+
+    private Transform target;
+    private int wavePointIndex = 0;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        target = WayPoint.points[0];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNext();
+        }
+	}
+
+    void GetNext()
+    {
+        if (wavePointIndex >= WayPoint.points.Length-1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wavePointIndex++;
+        target = WayPoint.points[wavePointIndex];
+    }
+
+
+}
---
+++ Assets/Scripts/Enemy/EnemySpawn.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+    public Transform enemy;
+    public Transform SpawnPoint;
+    public float timeOfSpawn;
+    public float countDown=2f;
+
+    private int waveIndex = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (countDown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countDown = timeOfSpawn;
+        }
+
+        countDown -= Time.deltaTime; 
+
+	}
+
+    IEnumerator SpawnWave()
+    {
+
+        waveIndex++;
+
+        for (int i = 0; i <= waveIndex; i++)
+        {
+            SpawEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+    }
+
+    void SpawEnemy()
+    {
+        Instantiate(enemy, SpawnPoint.position, SpawnPoint.rotation);
+    }
+
+}
